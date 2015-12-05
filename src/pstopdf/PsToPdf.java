28b72ba821bd6af0fff8ab902f64200d0f3c5960
janne <janne.j.hyvonen@gmail.com>
2015-12-05 23:27:18 +0200

    Nyt ohjelma lukee suorituskansiosta kaikki .ps tiedostot ja tulostaa ne samaan kansioon .pdf-tiedostoina. -Sallii maks. 99 .ps tiedostoa tai tulee virheilmoitus .ps tiedoston nimessä ei saa olla pistettä kuin lopussa osoittamassa tiedostoformaatin

--- src/pstopdf/PsToPdf.java
+++ src/pstopdf/PsToPdf.java
@@ -7,7 +7,7 @@ package pstopdf;
 
 /**
  *
- * @author Janne
+ * @author Janne Hyvönen 05.12.15
  */
 
 import java.io.File;
@@ -23,16 +23,56 @@ public class PsToPdf {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Initializing fos
         FileOutputStream fos = null;
+        
+        //Pointing to the location of Ghostscript binary to find .dll needed
         System.setProperty("jna.library.path", "C:\\Program Files (x86)\\gs\\gs9.05\\bin");
+        
+        //Get the absolut path to the folder where the file was executed
+        String path=System.getProperty("user.dir");
+        
+        //Make list of .ps files in the working directory
+        File file=new File(path);
+        File[] psList = new File[99];//Over 99 .ps files in the folder causes problem
+        File[] listOfFiles = file.listFiles();
+        int i=0;
+        for (File tiedosto : listOfFiles) {    
+            if(tiedosto.getName().endsWith(".ps")) {
+                psList[i]=(tiedosto);
+                i+=1;
+            }   
+        }
+        
+        //Iterate over the listed .ps files and create .pdf file from each 
+        i=0;
+        for (File tiedosto:psList){
         try{
  
             //load PostScript document
             PSDocument document = new PSDocument();
-            document.load(new File("C:\\Users\\Janne\\Desktop\\testi\\bell_206.ps"));
- 
+            document.load(psList[i]);
+            //System.out.println("Listassa:"+psList[i]); for testing
+            
+            i++;//add 1 to the list pointer
+            
+            //System.out.println("i on: "+i); for testing
+            
+            //Get absolut path to the file to be transformed
+            String name=psList[i-1].getAbsolutePath();
+            //System.out.println("name on:"+name); for testing
+            
+            //Take away the file extension
+            String[] extension=name.split("[.]");
+            //System.out.println("extensio on:"+extension[0]); for testing
+            
+            //Add new file extension
+            name=extension[0]+".pdf";
+            //System.out.println("Nimi:"+name); for testing
+            
             //create OutputStream
-            fos = new FileOutputStream(new File("C:\\Users\\Janne\\Desktop\\testi\\bell_206.pdf"));
+            fos = new FileOutputStream(new File(name));
  
             //create converter
             PDFConverter converter = new PDFConverter();
@@ -49,5 +89,5 @@ public class PsToPdf {
             IOUtils.closeQuietly(fos);
         }
     }
-    
+    }
 }
